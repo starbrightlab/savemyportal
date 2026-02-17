553e2959a6a7d147d0ce6a2162826fcd7def5181
starbrightlab <admin@starbrightlab.com>
2026-02-16 19:41:36 -0500

    Baseline: Remove Slideshow to isolate crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import React from 'react';
 import Layout from './components/Layout';
-import Slideshow from './components/Slideshow';
+// import Slideshow from './components/Slideshow';
 import './index.css';
 
 function App() {
   return (
     <Layout>
-      <Slideshow interval={10000} />
-      {/* Overlay content can go here if needed, but Slideshow is the main background/feature now */}
+      {/* <Slideshow interval={10000} /> */}
+      <div className="center-content">
+        <h1>SaveMyPortal</h1>
+        <p>Baseline Test: Heartbeat Only</p>
+      </div>
     </Layout>
   );
 }
